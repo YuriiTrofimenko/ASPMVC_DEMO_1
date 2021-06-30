b99176a9be734083f15a5f40b1bda2c0897bb752
student <student@DESKTOP-L3ET5DL>
2021-06-30 19:48:01 +0300

    ViewBag

--- ASPMVC_DEMO_1/Controllers/Default.cs
+++ ASPMVC_DEMO_1/Controllers/Default.cs
@@ -7,6 +7,8 @@ namespace ASPMVC_DEMO_1.Controllers
         // GET
         public ViewResult Index()
         {
+            ViewBag.Title = "ASP MVC Demo 1";
+            ViewBag.Content = "Default Page";
             return View();
         }
     }
