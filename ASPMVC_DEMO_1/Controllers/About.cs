ee20ec0713356616c46e38146a138ae9a88788b0
student <student@DESKTOP-L3ET5DL>
2021-06-30 19:36:11 +0300

    init n demo controllers step 1

---
+++ ASPMVC_DEMO_1/Controllers/About.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVC_DEMO_1.Controllers
+{
+    public class About : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "About Us ...";
+        }
+    }
+}
---
+++ ASPMVC_DEMO_1/Controllers/Default.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVC_DEMO_1.Controllers
+{
+    public class Default : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "Hello ASP.NET MVC!";
+        }
+    }
+}
